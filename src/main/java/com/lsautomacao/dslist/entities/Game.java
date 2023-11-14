a543a4c58a6bbff7adddc9dc35673b25945a4cbe
Leonardo Santos <leolmblsantos@gmail.com>
2023-11-14 00:05:47 -0300

    properties, Game, ORM

---
+++ src/main/java/com/lsautomacao/dslist/entities/Game.java
@@ -0,0 +1,28 @@
+package com.lsautomacao.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_game")
+@Data
+public class Game {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platform;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+
+}
